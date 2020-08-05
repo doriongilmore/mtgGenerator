17335f115d9187f65ba0d6ab3e95a8edcc8783fd
doriongilmore <doriongilmore@outlook.com>
2020-08-06 01:13:19 +0200

    small cleanup

--- src/utils/consts/modals.js
+++ src/utils/consts/modals.js
@@ -32,9 +32,7 @@ const modals = {
   },
   confirmationMessage: {
     deckLost: 'The deck will be lost permanently!',
-    allDecksLost:
-      'All you decks will be lost permanently and the "Classic List" will return to it\'s default state.' +
-      ' This can resolve some issues due to bad retro-compatibility during version update (probably of this site) ',
+    allDecksLost: 'All you decks will be lost permanently and the "Classic List" will return to it\'s default state.',
   },
   events: {
     vue: {
