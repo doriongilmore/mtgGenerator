129f43b83cf736f9b03b91685101c9faa41f0c40
doriongilmore <doriongilmore@outlook.com>
2020-08-15 14:17:40 +0200

    common webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+});
